87a283c1a724d5987737077a6a3aa30fbe911188
h5avlov <h5avlov@abv.bg>
2025-07-17 06:32:38 +0300

    Handlebars configured.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express'); 
+const handlebars = require('express-handlebars'); 
 // Configuration imports 
 const exp = require('./config/express.config');
+const hbs = require('./config/handlebars.config');
 
 const app = express(); 
 
-// Configurations
+// Configurations 
 exp.config(app); 
+hbs.config(app); 
 
+
+// Routes 
 app.get('/', (req, res) => {
-    res.send('Works.'); 
+    res.render('home'); 
 }); 
 
+
+// Server start 
 app.listen(4000, () => console.log('Server listening on port 4000...')); 
