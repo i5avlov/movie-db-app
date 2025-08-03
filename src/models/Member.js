4ada8f7c06f7612d98e693da12861705300ad68f
h5avlov <h5avlov@abv.bg>
2025-08-03 09:45:03 +0300

    Schema method to calculate member age removed

--- src/models/Member.js
+++ src/models/Member.js
@@ -36,30 +36,6 @@ const memberSchema = new Schema({
 
 }); 
 
-memberSchema.methods = { 
-    // Returns member age 
-    getAge: function() { 
-        // Today date, month and day 
-        const now = new Date(Date.now()); 
-        const monthNow = now.getMonth(); 
-        const dateNow = now.getDate(); 
-
-        // Member date, month and day of birth 
-        const birthdate = new Date(this.birthdate); 
-        const monthOfBirth = birthdate.getMonth(); 
-        const dateOfBirth = birthdate.getDate(); 
-
-        // Years from member birthdate to today 
-        const age = now.getFullYear() - birthdate.getFullYear(); 
-        // Member birthday this year not yet celebrated 
-        if (monthNow < monthOfBirth || (monthNow === monthOfBirth && dateNow < dateOfBirth)) { 
-            age -= 1; 
-        } 
-
-        return age; 
-    }
-}
-
 const Member = model('Member', memberSchema); 
 
 module.exports = Member; 
