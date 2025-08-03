f17bc9fd0d84d1ada58fa39a107532df29d3e6ff
h5avlov <h5avlov@abv.bg>
2025-08-03 14:25:51 +0300

    Method to get all categories added

--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -1,6 +1,10 @@
 const Category = require('../models/Category'); 
 
 module.exports = { 
+    getAll: () => { 
+        return Category.find({}); 
+    }, 
+
     add: (categoryData) => { 
         const { name, description, imageUrl } = categoryData; 
         
