bab59456a510757551329d9fb32ffaefb6e8af35
h5avlov <h5avlov@abv.bg>
2025-08-03 10:08:50 +0300

    Genres controller added to routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const routes = require('express').Router(); 
+const genresController = require('./controllers/genres.controller');
 const homeController = require('./controllers/home.controller'); 
 const membersController = require('./controllers/members.controller');
 const occupationsController = require('./controllers/occupations.controller'); 
@@ -7,5 +8,6 @@ const occupationsController = require('./controllers/occupations.controller');
 routes.use(homeController); 
 routes.use('/occupations', occupationsController); 
 routes.use('/members', membersController); 
+routes.use('/genres', genresController); 
 
 module.exports = routes; 
