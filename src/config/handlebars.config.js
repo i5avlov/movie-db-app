4be948359d91b798f388da539126b9e7b13c1885
h5avlov <h5avlov@abv.bg>
2025-08-03 09:43:26 +0300

    Handlebars helpers to get member age and name added

--- src/config/handlebars.config.js
+++ src/config/handlebars.config.js
@@ -1,8 +1,12 @@
 const handlebars = require('express-handlebars'); 
+const helpers = require('../helpers/handlebars.helpers'); 
 
 module.exports = { 
     config: (app) => { 
-        app.engine('hbs', handlebars.engine({ extname: 'hbs' })); 
+        app.engine('hbs', handlebars.engine({ 
+            extname: 'hbs', 
+            helpers: helpers 
+        })); 
         app.set('view engine', 'hbs'); 
         app.set('views', 'src/views'); 
     } 
---
+++ src/helpers/handlebars.helpers.js
@@ -0,0 +1,27 @@
+module.exports = {
+    getMemberName: (firstName, lastName) => { 
+        return `${firstName} ${lastName}`; 
+    }, 
+
+    getMemberAge: (birthDate) => {
+         // Today date, month and day 
+        const now = new Date(Date.now()); 
+        const monthNow = now.getMonth(); 
+        const dateNow = now.getDate(); 
+
+        // Member date, month and day of birth 
+        const birthdate = new Date(birthDate); 
+        const monthOfBirth = birthdate.getMonth(); 
+        const dateOfBirth = birthdate.getDate(); 
+
+        // Years from member birthdate to today 
+        let age = now.getFullYear() - birthdate.getFullYear(); 
+        // Member birthday this year not yet celebrated 
+        if (monthNow < monthOfBirth || (monthNow === monthOfBirth && dateNow < dateOfBirth)) { 
+            age -= 1; 
+        } 
+
+        return age; 
+    }, 
+
+}; 
